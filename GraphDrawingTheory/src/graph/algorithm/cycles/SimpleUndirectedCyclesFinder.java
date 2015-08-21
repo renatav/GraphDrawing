3dfe92dcc94a456b7f2533c0b4d646d2ce7a2754
Renata <vrenata8@gmail.com>
2015-08-21 15:31:53 +0200

    Finished cycles finder

--- GraphDrawingTheory/src/graph/algorithm/cycles/SimpleUndirectedCyclesFinder.java
+++ GraphDrawingTheory/src/graph/algorithm/cycles/SimpleUndirectedCyclesFinder.java
@@ -11,8 +11,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
-import javax.swing.JPopupMenu;
-
 public class SimpleUndirectedCyclesFinder<V extends Vertex, E extends Edge<V>> {
 
 
@@ -49,7 +47,6 @@ public class SimpleUndirectedCyclesFinder<V extends Vertex, E extends Edge<V>> {
 		List<List<V>> currentNew = new ArrayList<List<V>>();
 		List<List<V>> containingEdge = new ArrayList<List<V>>();
 		Map<List<V>, List<List<V>>> joinedCycles = new HashMap<List<V>, List<List<V>>>();
-		int index = 0;
 
 		do{
 
@@ -60,110 +57,135 @@ public class SimpleUndirectedCyclesFinder<V extends Vertex, E extends Edge<V>> {
 				V v2 = e.getOrigin();
 				Set<List<V>> containingV1 = cyclesContainingVertex.get(v1);
 				Set<List<V>> containingV2 = cyclesContainingVertex.get(v2);
+
 				for (List<V> cycle : containingV1){
 					if (!containingV2.contains(cycle))
 						continue;
 					containingEdge.add(cycle);
 				}
-				if (containingEdge.size() < 2)
-					continue; //nothing to join
 
-				//now join two blocks
-				for (int i = 0; i < containingEdge.size() - 1; i++){
-					for (int j = i + 1; j < containingEdge.size(); j++){
-						List<V> cycle1 =  containingEdge.get(i);
-						List<V> cycle2 = containingEdge.get(j);
 
-						boolean joined = false;
+				if (containingEdge.size() >= 2)
+					for (int i = 0; i < containingEdge.size() - 1; i++){
+						for (int j = i + 1; j < containingEdge.size(); j++){
+							List<V> cycle1 =  containingEdge.get(i);
+							List<V> cycle2 = containingEdge.get(j);
 
-						if (joinedCycles.containsKey(cycle1)){
-							if (joinedCycles.get(cycle1).contains(cycle2))
-								joined = true;
-						}
-						if (!joined){
-							if (joinedCycles.containsKey(cycle2))
-								if (joinedCycles.get(cycle2).contains(cycle1))
+							boolean joined = false;
+
+							if (joinedCycles.containsKey(cycle1)){
+								if (joinedCycles.get(cycle1).contains(cycle2))
 									joined = true;
+							}
+							if (!joined){
+								if (joinedCycles.containsKey(cycle2))
+									if (joinedCycles.get(cycle2).contains(cycle1))
+										joined = true;
+							}
+
+							if (joined)
+								continue;
+
+
+							//is v1, v2 edge in cycle 1
+
+							boolean isEdgeInCycle1 = false;
+							boolean isEdgeInCycle2 = false;
+
+							int v1index = cycle1.indexOf(v1);
+							int v2index = cycle1.indexOf(v2);
+							int dif = Math.abs(v1index - v2index);
+							if (dif == 1 || dif == cycle1.size() - 1)
+								isEdgeInCycle1 = true;
+
+							v1index = cycle2.indexOf(v1);
+							v2index = cycle2.indexOf(v2);
+							dif = Math.abs(v1index - v2index);
+							if (dif == 1 || dif == cycle2.size() - 1)
+								isEdgeInCycle2 = true;
+
+							if (!isEdgeInCycle1 && !isEdgeInCycle2)
+								continue;
+
+							List<V> resultingCycle = null;
+
+							if (isEdgeInCycle1 && isEdgeInCycle2){
+
+								//the cycles share and edge
+								//try to join them
+
+								if (!shouldJoinCycles(cycle1, cycle2, v1, v2))
+									continue;
+
+								resultingCycle = joinCycles(cycle1, cycle2, v1, v2);
+
+							}
+
+							else{
+
+								//one has vertices as an edge
+								//the other has them too, but they are not directly linked
+								//see if these cycles should be subtracted
+								//the one that does not contain the edge should be bigger
+								//and formed by joining the one that contains it with
+								//another that contains it
+								List<V> bigger = cycle1;
+								List<V> smaller = cycle2;
+								if (!isEdgeInCycle2){
+									bigger = cycle2;
+									smaller = cycle1;
+								}
+
+								if (shouldTryToSubtractCycles(bigger, smaller)){
+									resultingCycle = subtractCycles(bigger, smaller, v1, v2);
+								}
+
+							}
+
+							if (resultingCycle == null)
+								continue;
+
+							List<V> existing = alreadyExists(resultingCycle);
+
+							if (existing == null){
+								currentNew.add(resultingCycle);
+								existing = resultingCycle;
+							}
+
+
+							List<List<V>> list1 = joinedCycles.get(cycle1);
+							List<List<V>> list2 = joinedCycles.get(cycle2);
+
+							if (list1 == null){
+								list1 = new ArrayList<List<V>>();
+								joinedCycles.put(cycle1, list1);
+							}
+
+							if (list2 == null){
+								list2 = new ArrayList<List<V>>();
+								joinedCycles.put(cycle2, list2);
+							}
+
+
+							list1.add(cycle2);
+							list2.add(cycle1);
+
+							//to avoid subtraction between joined cycle and the cycles it was made from
+							//add it to the joined list as well
+							list1.add(existing);
+							list2.add(existing);
+
+
+							for (V v : existing)
+								cyclesContainingVertex.get(v).add(existing);
 						}
-
-						if (joined)
-							continue;
-
-						boolean join = shouldJoinCycles(cycle1, cycle2, v1, v2);
-						boolean subtract = false;
-						if (!join)
-							subtract = shouldTryToSubtractCycles(cycle1, cycle2);
-
-						//check if cycles should be joined
-						if (!join && !subtract) 
-							continue;
-
-
-						List<V> resultingCycle;
-
-						if	(join)
-							resultingCycle = joinCycles(cycle1, cycle2, v1, v2);
-						else{ 
-							resultingCycle = subtractCycles(cycle1, cycle2, v1, v2);
-							//continue;
-						}
-						
-						if (resultingCycle == null)
-							continue;
-
-						
-							System.out.println("edge: " + e);
-						System.out.println(cycle1 + " and " + cycle2);
-
-						if (join)
-							System.out.println("Joined cycle: " + resultingCycle);
-						if (subtract){
-							System.out.println("Subtracted cycle: " + resultingCycle);
-						}
-
-						//TODO check if cycle is a permutation of an existing one!
-						if (!alreadyExists(resultingCycle))
-							currentNew.add(resultingCycle);
-
-
-						List<List<V>> list1 = joinedCycles.get(cycle1);
-						List<List<V>> list2 = joinedCycles.get(cycle2);
-
-						if (list1 == null){
-							list1 = new ArrayList<List<V>>();
-							joinedCycles.put(cycle1, list1);
-						}
-
-						if (list2 == null){
-							list2 = new ArrayList<List<V>>();
-							joinedCycles.put(cycle2, list2);
-						}
-
-
-						list1.add(cycle2);
-						list2.add(cycle1);
-
-						//to avoid subtraction between joined cycle and the cycles it was made from
-						//add it to the joined list as well
-						list1.add(resultingCycle);
-						list2.add(resultingCycle);
-
-
-						for (V v : resultingCycle)
-							cyclesContainingVertex.get(v).add(resultingCycle);
-
 					}
-				}
+
+
+
 			}
 			ret.addAll(currentNew);
 			//System.out.println(currentNew);
-			
-			//TODO ODVOJITI ODUZIMANJE I JOIN-OVANJE!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!
-			//za oduzimanje treba uzeti par
-			//pogledati da li se mogu oduzeti po nekoj ivici
-			//i onda
-			//ne traziti po tome da sadrze oba nku ivicu
-			//posto nece!
 
 		}
 
@@ -250,6 +272,10 @@ public class SimpleUndirectedCyclesFinder<V extends Vertex, E extends Edge<V>> {
 
 	private List<V> subtractCycles(List<V> cycle1, List<V> cycle2, V edgeStart, V edgeEnd){
 
+		//System.out.println("edge " + edgeStart + " " + edgeEnd);
+		//System.out.println("Subtracting " + cycle1 + " and " + cycle2);
+
+
 		List<V> bigger = cycle1.size() > cycle2.size() ? cycle1 : cycle2;
 		List<V> smaller = bigger == cycle1 ? cycle2 : cycle1;
 
@@ -257,7 +283,7 @@ public class SimpleUndirectedCyclesFinder<V extends Vertex, E extends Edge<V>> {
 		for (V v : smaller)
 			if (v != edgeStart && v != edgeEnd)
 				ret.remove(v);
-		
+
 		//check if that is a cycle
 		for (int i = 0; i < ret.size(); i++){
 			V v1 = ret.get(i);
@@ -270,11 +296,9 @@ public class SimpleUndirectedCyclesFinder<V extends Vertex, E extends Edge<V>> {
 		return ret;
 	}
 
-	private boolean shouldTryToSubtractCycles(List<V> cycle1, List<V> cycle2){
-		List<V> bigger = cycle1.size() > cycle2.size() ? cycle1 : cycle2;
-		List<V> smaller = bigger == cycle1 ? cycle2 : cycle1;
-		
-		if (bigger.size() == smaller.size())
+	private boolean shouldTryToSubtractCycles(List<V> bigger, List<V> smaller){
+
+		if (bigger.size() <= smaller.size())
 			return false;
 
 		for (V v : smaller)
@@ -298,44 +322,70 @@ public class SimpleUndirectedCyclesFinder<V extends Vertex, E extends Edge<V>> {
 
 	}
 
-	private boolean alreadyExists(List<V> newCycle){
+	private List<V> alreadyExists(List<V> newCycle){
 
 		Set<List<V>> cycles = cyclesContainingVertex.get(newCycle.get(0));
+		Set<List<V>> result = new HashSet<List<V>>(cycles);
 
 		for (int i = 1; i < newCycle.size(); i++)
-			cycles.retainAll(cyclesContainingVertex.get(newCycle.get(i)));
+			result.retainAll(cyclesContainingVertex.get(newCycle.get(i)));
 
-		System.out.println("Remaining cycles: " + cycles);
+		//		System.out.println("Remaining cycles: " + cycles);
+
 
 		for (List<V> cycle : cycles){
 			if (cycle.size() != newCycle.size())
 				continue;
 
 			//now check order of vertices
-			int indexCycle = cycle.indexOf(newCycle.get(0));
-			int indexNewCycle = 0;
+			int indexCycle = (cycle.indexOf(newCycle.get(0)) + 1) % cycle.size();
+			int indexNewCycle = 1;
 			boolean permutation = true;
-
+			boolean reverse = false;
+			boolean first = true;
+			
+			//System.out.println("Comparing " + newCycle + "and " + cycle);
+			
 			while (indexNewCycle < newCycle.size()){
-				if (cycle.get(indexCycle) != newCycle.get(indexCycle)){
-					permutation = false;
-					break;
+				
+				if (cycle.get(indexCycle) != newCycle.get(indexNewCycle)){
+					if (first){
+						//reset and try the other way
+						reverse = true;
+						first = false;
+						indexCycle = cycle.indexOf(newCycle.get(0));
+						indexNewCycle = 0;
+						continue;
+					}
+					else{
+						permutation = false;
+						break;
+					}
 				}
-
-				indexCycle ++;
+				first = false;
 				indexNewCycle ++;
-				indexCycle = indexCycle % cycle.size();
+				if (!reverse){
+					indexCycle ++;	
+					indexCycle = indexCycle % cycle.size();
+				}
+				else{
+					indexCycle --;
+					if (indexCycle == -1)
+						indexCycle = cycle.size() - 1;
+				}
 			}
+
 			if (permutation){
-			//	System.out.println(newCycle + " is permutation of " + cycle);
-				return true;
+				//System.out.println(newCycle + " is permutation of " + cycle);
+				return cycle;
 			}
 
 		}
 
 
-		return false;
+		return null;
 	}
+
 
 
 }
